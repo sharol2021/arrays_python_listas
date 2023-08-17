9671ca5421f558d13884d2de08b52439b3d22bcc
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-08-17 16:21:35 -0500

    se agrego el ejercicio_6.py

---
+++ ejercicio_6.py
@@ -0,0 +1,26 @@
+resultado = 0
+
+# input
+n = int(input("Número de elementos de los vectores: "))
+        
+# processing
+vector1 = [None] * n
+
+print("Elementos del segundo vector:")
+for i in range(n):
+  print("Posición:", i)
+  vector1[i] = int(input("Valor: "))
+
+vector2 = [None] * n
+print("Elementos del segundo vector:")
+for i in range(n):
+  print("Posición:", i)
+  vector2[i] = int(input("Valor: "))
+
+for i in range(n):
+  producto += vector2[i]*vector1[i]
+
+# output
+print("Vector 1:", vector1)
+print("Vector 2:", vector2)
+print("El producto punto entre los dos vectores es:", resultado)
