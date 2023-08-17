2443089667d8cef3afa5d68fba2cc085ea36e3a2
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-08-17 16:27:13 -0500

    se agrego el ejercicio_8.py

---
+++ ejercicio_8.py
@@ -0,0 +1,18 @@
+import random
+
+M = []
+
+# input
+m = int(input("Filas de la matriz: "))
+n = int(input("Columnas de la matriz: "))
+
+# processing
+for i in range(m):
+    M.append([])
+    for j in range(n):
+        M[i].append(random.randint(1,9))
+
+for k in range(m):
+    for j in range(n):
+        print(M[k][j], end=" ") # output
+    print()
